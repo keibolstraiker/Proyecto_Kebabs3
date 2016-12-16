8404c19fc42cf7f465fa9402104cafae6e2ae8e8
keibolstraiker <ruben_374@hotmail.com>
2016-12-16 12:46:00 +0100

    Asignación del Mapa, aún no funciona aparentemente

--- app/src/main/java/com/alumno/proyecto_kebabs/PantallaPreviaInicio.java
+++ app/src/main/java/com/alumno/proyecto_kebabs/PantallaPreviaInicio.java
@@ -2,21 +2,32 @@ package com.alumno.proyecto_kebabs;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class PantallaPreviaInicio extends AppCompatActivity {
 
+public class PantallaPreviaInicio extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
     private TextView telefono;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_pantalla_previa_inicio);
-
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         telefono = (TextView) findViewById(R.id.lblTelefono);
 
@@ -28,12 +39,21 @@ public class PantallaPreviaInicio extends AppCompatActivity {
                 Intent intent = new Intent(Intent.ACTION_CALL);
 
                 intent.setData(Uri.parse("tel:943465768"));
-                startActivity(intent);
+
 
             }
 
 
         });
     }
-}
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+    }
+
 
