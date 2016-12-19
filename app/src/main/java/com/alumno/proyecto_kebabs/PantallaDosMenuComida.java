464bb657a11516bc5586b3b38914fae33600edfa
keibolstraiker <ruben_374@hotmail.com>
2016-12-19 11:06:46 +0100

    He añadido la funcionalidad a todos los spiners, solo falta que se añadan al arrai list que hace el pedido

--- app/src/main/java/com/alumno/proyecto_kebabs/PantallaDosMenuComida.java
+++ app/src/main/java/com/alumno/proyecto_kebabs/PantallaDosMenuComida.java
@@ -6,46 +6,105 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class PantallaDosMenuComida extends AppCompatActivity {
-    private Spinner cmbTamaño;
+    private Spinner cmbTipo_tamaño;
+    private Spinner cmbTipo_carne;
+    private Spinner cmbTipo_kebab;
     private TextView lblPedido;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_pantalla_dos_menu_comida);
 
-
-
-
-        ArrayAdapter<CharSequence> adaptador =
-                ArrayAdapter.createFromResource
-                        (this, R.array.tamaño, android.R.layout.simple_spinner_item);
-        cmbTamaño = (Spinner) findViewById (R.id.cmbTamaño);
+        cmbTipo_tamaño = (Spinner) findViewById (R.id.cmbTamaño);
         lblPedido = (TextView) findViewById (R.id.lblPedido);
+        cmbTipo_kebab = (Spinner) findViewById (R.id.cmbTipoKebab);
+        cmbTipo_carne = (Spinner) findViewById (R.id.cmbTipoCarne);
 
-        adaptador.setDropDownViewResource(
+        ArrayAdapter<CharSequence> adaptadorKebab =
+                ArrayAdapter.createFromResource
+                        (this, R.array.tipo_kebab, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adaptadorCarne =
+                ArrayAdapter.createFromResource
+                        (this, R.array.tipo_carne, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adaptadorTamaño =
+                ArrayAdapter.createFromResource
+                        (this, R.array.tipo_tamaño, android.R.layout.simple_spinner_item);//el array adapter esta señalando al array de strings llamado tamaños
+
+
+        adaptadorKebab.setDropDownViewResource(
                 android.R.layout.simple_spinner_dropdown_item);
-        cmbTamaño.setAdapter(adaptador);
+        cmbTipo_kebab.setAdapter(adaptadorTamaño);
 
 
-        cmbTamaño.setOnItemSelectedListener(
+        cmbTipo_kebab.setOnItemSelectedListener(
                 new AdapterView.OnItemSelectedListener() {
                     public void onItemSelected(AdapterView<?> parent,
-                                               android.view.View v, int position, long id) {
+                                               android.view.View v, int position, long id) {// el parametro posición se va a posicionar en el item del array exacto del spiner  el cual pinche el usuario
                         if       (position==0) {
-                            lblMensaje.setText("Que tal?");
+                            //aquí habra que meter en el arrai list que va recabando toda la informacion del pedido el Doner.
                         }else if (position==1){
-                            lblMensaje.setText("Zer moduz?");
+                            //aquí habra que meter en el arrai list que va recabando toda la informacion del pedido el durum.
+                        }else if (position==2){
+                            //aquí habra que meter en el arrai list que va recabando toda la informacion del pedido el lamhacum.
+                        }else if (position==3){
+                            //aquí habra que meter en el arrai list que va recabando toda la informacion del pedido el Shawarma.
+                        }else if (position==4){
+                            //aquí habra que meter en el arrai list que va recabando toda la informacion del pedido el Gyros.
                         }
 
                     }
                     public void onNothingSelected(AdapterView<?> parent) {
-                        lblMensaje.setText("Selecciona algun idioma");
+                        Toast.makeText(getApplicationContext(), "Por favor, debe seleccionar una opción",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+        adaptadorCarne.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+        cmbTipo_carne.setAdapter(adaptadorTamaño);
+
+        cmbTipo_carne.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> parent,
+                                               android.view.View v, int position, long id) {// el parametro posición se va a posicionar en el item del array exacto del spiner  el cual pinche el usuario
+                        if       (position==0) {
+                            //aquí habra que meter en el arrai list que va recabando toda la informacion del pedido el ternera.
+                        }else if (position==1){
+                            //aquí habra que meter en el arrai list que va recabando toda la informacion del pedido el pollo.
+                        }else if (position==2){
+                            //aquí habra que meter en el arrai list que va recabando toda la informacion del pedido el cordero.
+                        }
+
+                    }
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        Toast.makeText(getApplicationContext(), "Por favor, debe seleccionar una opción",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+        adaptadorTamaño.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+        cmbTipo_tamaño.setAdapter(adaptadorTamaño);
+
+        cmbTipo_tamaño.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> parent,
+                                               android.view.View v, int position, long id) {// el parametro posición se va a posicionar en el item del array exacto del spiner  el cual pinche el usuario
+                        if       (position==0) {
+                            //aquí habra que meter en el arrai list que va recabando toda la informacion del pedido el tamaño normal.
+                        }else if (position==1){
+                            //aquí habra que meter en el arrai list que va recabando toda la informacion del pedido el tamaño completo.
+                        }
+
+                    }
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        Toast.makeText(getApplicationContext(), "Por favor, debe seleccionar una opción",
+                                Toast.LENGTH_LONG).show();
                     }
                 });
     }
 
 }
-}
+
