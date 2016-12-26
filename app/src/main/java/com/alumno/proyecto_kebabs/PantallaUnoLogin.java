5922f5adbb244fbdae98f6f6ede0484779c3ebc1
Gari2017 <garimartinez00@hotmail.com>
2016-12-26 11:43:56 +0100

    He corregido el problemilla de la pantalla uno con el arraylist, era una tontería

--- app/src/main/java/com/alumno/proyecto_kebabs/PantallaUnoLogin.java
+++ app/src/main/java/com/alumno/proyecto_kebabs/PantallaUnoLogin.java
@@ -73,7 +73,8 @@ public class PantallaUnoLogin extends AppCompatActivity {
             }
         });
     }
-public void LanzarActividadDos(String[] datos){
+public void LanzarActividadDos(ArrayList<String> datos){
+
 
 
 }
